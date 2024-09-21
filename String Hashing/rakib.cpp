6600c7c8f40016feee0261acbd1007e6e4a5a894
AkashAhmed66 <akashahmed662001@gmail.com>
2024-09-21 13:16:41 +0600

    update

--- String Hashing/rakib.cpp
+++ String Hashing/rakib.cpp
@@ -1,69 +1,78 @@
 #include "bits/stdc++.h"
 using namespace std;
+typedef long long ll;
 
-const int MAX = 5e5 + 5;
-vector<long long> MOD = {127657753, 987654319};
-vector<array<long long, 2>> pwr(MAX), ipwr(MAX);
-array<long long, 2> bs = {137, 277};
+const int N = 5e5 + 5, T = 2;
+vector<ll> mod = {127657753, 987654319};
+array<ll, T> base = {137, 277};
+vector<array<ll, T>> bpwr(N), ibpwr(N);
 
-long long bigMod(long long base, long long power, int m) {
-  long long res = 1;
-  while (power) {
-    if (power & 1LL) {
-      (res *= base) %= MOD[m];
+ll bigMod(ll b, ll p, ll m){
+    if(p == 0)return 1LL ;
+    ll ans = bigMod(b, p/2, m);
+    ans = (ans * ans) % mod[m];
+    if(p % 2 == 1){
+        ans = (ans * b) % mod[m];
     }
-    (base *= base) %= MOD[m];
-    power /= 2;
-  }
-  return res;
+    return ans;
 }
 
 void Pre_Calculate() {
-  pwr[0][0] = pwr[0][1] = 1;
-  for (int i = 1; i < MAX; i++) {
-    (pwr[i][0] = pwr[i - 1][0] * bs[0]) %= MOD[0];
-    (pwr[i][1] = pwr[i - 1][1] * bs[1]) %= MOD[1];
+  for(int i = 0; i < T; i++){
+    bpwr[0][i] = 1;
   }
-  ipwr[0][0] = ipwr[0][1] = 1;
-  long long ip1 = bigMod(bs[0], MOD[0] - 2, 0);
-  long long ip2 = bigMod(bs[1], MOD[1] - 2, 1);
-  for (int i = 1; i < MAX; i++) {
-    ipwr[i][0] = (ipwr[i - 1][0] * ip1) % MOD[0];
-    ipwr[i][1] = (ipwr[i - 1][1] * ip2) % MOD[1];
+  for (int i = 1; i < N; i++) {
+    for(int j = 0; j < T; j++){
+      (bpwr[i][j] = bpwr[i - 1][j] * base[j]) %= mod[j];
+    }
+  }
+  ll ip[T]{};
+  for(int i = 0; i < T; i++){
+    ibpwr[0][i] = 1;
+    ip[i] = bigMod(base[i], mod[i] - 2, i);
+  }
+  for (int i = 1; i < N; i++) {
+    for(int j = 0; j < T; j++){
+        ibpwr[i][j] = (ibpwr[i - 1][j] * ip[j]) % mod[j];
+    }
   }
 }
 
 struct Hashing {
   int n;
   string s;
-  vector<array<long long, 2>> hs;
+  vector<array<ll, T>> hs;
   Hashing (string t) {
     n = t.size();
     s = t;
     hs.resize(n + 1);
     for (int i = 0; i < n; i++) {
-      (hs[i + 1][0] = hs[i][0] + (pwr[i][0] * s[i]) % MOD[0]) %= MOD[0];
-      (hs[i + 1][1] = hs[i][1] + (pwr[i][1] * s[i]) % MOD[1]) %= MOD[1];
+      for(int j = 0; j < T; j++){
+        (hs[i + 1][j] = hs[i][j] + (bpwr[i][j] * s[i]) % mod[j]) %= mod[j];
+      }
     }
   }
-  array<long long, 2> get_hash(int l, int r) {
+  array<ll, T> get_hash(int l, int r) {
     if (l > r) return {0, 0};
-    long long res1 = ((hs[r][0] - hs[l - 1][0]) + MOD[0]) % MOD[0];
-    long long res2 = ((hs[r][1] - hs[l - 1][1]) + MOD[1]) % MOD[1];
-    (res1 *= ipwr[l - 1][0]) %= MOD[0];
-    (res2 *= ipwr[l - 1][1]) %= MOD[1];
-    return {res1, res2};
-  }
-  auto merge(array<long long, 2> left, array<long long, 2> right, int left_sz) {
-    for (int i = 0; i < 2; i++) {
-      (right[i] *= pwr[left_sz][i]) %= MOD[i];
-      (left[i] += right[i]) %= MOD[i];
+    array<ll, T> res;
+    for(int i = 0; i < T; i++){
+      res[i] = ((hs[r][i] - hs[l - 1][i]) + mod[i]) % mod[i];
+      (res[i] *= ibpwr[l - 1][i]) %= mod[i];
     }
-    return left;
+    return res;
+  }
+  auto Merge(array<ll, T> l, array<ll, T> r, int l_sz) {
+    for (int i = 0; i < T; i++) {
+      (r[i] *= bpwr[l_sz][i]) %= mod[i];
+      (l[i] += r[i]) %= mod[i];
+    }
+    return l;
   };
 };
 
 void solve() {
+  string s;
+  cin >> s;
   //create hash
   Hashing hsh(s);
   //get hash
@@ -71,10 +80,11 @@ void solve() {
 }
 
 int main() {
-  cin.tie(0)->sync_with_stdio(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   int t = 1;
   Pre_Calculate();
-  cin >> t;
+  //cin >> t;
   for(int i = 1; i <= t; i++){
     solve();
   }
